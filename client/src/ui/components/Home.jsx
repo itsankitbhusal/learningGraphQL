369fa26b6476463bacf8fe3c2727b2a1b163b8b1
ankitbhusal <ankitbhusal95@gmail.com>
2023-04-03 22:21:28 +0545

    material ui && homepage simple design

--- client/src/ui/components/Home.jsx
+++ client/src/ui/components/Home.jsx
@@ -1,21 +1,69 @@
 import React from "react";
+import { Container, Typography } from "@mui/material";
+import { Box, Stack } from "@mui/system";
 
 const Home = () => {
   return (
-    <>
-      <blockquote>
-        <h5>if it works don't touch it</h5>
-        <h6>~Ankit</h6>
-      </blockquote>
-      <blockquote>
-        <h5>if it works don't touch it</h5>
-        <h6>~Ankit</h6>
-      </blockquote>
-      <blockquote>
-        <h5>if it works don't touch it</h5>
-        <h6>~Sagar</h6>
-      </blockquote>
-    </>
+    <Container>
+      <Stack spacing={"1"} sx={{ border: 1, borderRadius: 1 }}>
+        <div>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            sx={{
+              border: 1,
+              borderRadius: 1,
+              m: 2,
+              px: 2,
+              "&:hover": {
+                backgroundColor: "primary.main",
+                opacity: [0.9, 0.8, 0.7],
+              },
+              flexDirection: "row",
+            }}
+          >
+            <h5>if it works don't touch it</h5>
+            <h6>~Ankit</h6>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            sx={{
+              border: 1,
+              borderRadius: 1,
+              m: 2,
+              px: 2,
+              "&:hover": {
+                backgroundColor: "primary.main",
+                opacity: [0.9, 0.8, 0.7],
+              },
+              flexDirection: "row",
+            }}
+          >
+            <h5>if it works don't touch it</h5>
+            <h6>~Ankit</h6>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            sx={{
+              border: 1,
+              borderRadius: 1,
+              m: 2,
+              px: 2,
+              "&:hover": {
+                backgroundColor: "primary.main",
+                opacity: [0.9, 0.8, 0.7],
+              },
+              flexDirection: "row",
+            }}
+          >
+            <h5>if it works don't touch it</h5>
+            <h6>~Sagar</h6>
+          </Box>
+        </div>
+      </Stack>
+    </Container>
   );
 };
 
