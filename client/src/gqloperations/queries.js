d2eeab3ee150dccc685014abeb15037dbe13e2ea
ankitbhusal <ankitbhusal95@gmail.com>
2023-04-09 21:23:25 +0545

    proper way to consume GraphQL api

---
+++ client/src/gqloperations/queries.js
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client"
+export const GET_ALL_QUOTES = gql`
+query{
+    quotes{
+      name
+      by
+      user{
+        email
+      }
+    }
+  }`;
--- client/src/ui/components/Home.jsx
+++ client/src/ui/components/Home.jsx
@@ -1,94 +1,43 @@
-import React from "react";
-import { useEffect } from "react";
 import { Container } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 
-const Home = () => {
-  useEffect(() => {
-    const foo = async () => {
-      const res = await fetch("http://localhost:4000/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "Application/json",
-        },
-        body: JSON.stringify({
-          query: `
-          query{
-            quotes{
-              name
-              by
-              user{
-                email
-              }
-            }
-          }
-        `,
-        }),
-      });
+import { useQuery } from "@apollo/client";
+import { GET_ALL_QUOTES } from "../../gqloperations/queries";
 
-      const data = await res.json();
-      console.log(data);
-    };
-    foo();
-  }, []);
+const Home = () => {
+  const { loading, error, data } = useQuery(GET_ALL_QUOTES);
+  data && console.log(data);
+
+  // loading && <h1>Loading ...</h1>;
+  // error && console.log(error.message);
+
   return (
     <Container>
       <Stack spacing={"1"} sx={{ border: 1, borderRadius: 1 }}>
         <div>
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            sx={{
-              border: 1,
-              borderRadius: 1,
-              m: 2,
-              px: 2,
-              "&:hover": {
-                backgroundColor: "primary.main",
-                opacity: [0.9, 0.8, 0.7],
-              },
-              flexDirection: "row",
-            }}
-          >
-            <h5>if it works don't touch it</h5>
-            <h6>~Ankit</h6>
-          </Box>
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            sx={{
-              border: 1,
-              borderRadius: 1,
-              m: 2,
-              px: 2,
-              "&:hover": {
-                backgroundColor: "primary.main",
-                opacity: [0.9, 0.8, 0.7],
-              },
-              flexDirection: "row",
-            }}
-          >
-            <h5>if it works don't touch it</h5>
-            <h6>~Ankit</h6>
-          </Box>
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            sx={{
-              border: 1,
-              borderRadius: 1,
-              m: 2,
-              px: 2,
-              "&:hover": {
-                backgroundColor: "primary.main",
-                opacity: [0.9, 0.8, 0.7],
-              },
-              flexDirection: "row",
-            }}
-          >
-            <h5>if it works don't touch it</h5>
-            <h6>~Sagar</h6>
-          </Box>
+          {data &&
+            data.quotes.map((quote) => {
+              return (
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  sx={{
+                    border: 1,
+                    borderRadius: 1,
+                    m: 2,
+                    px: 2,
+                    "&:hover": {
+                      backgroundColor: "primary.main",
+                      opacity: [0.9, 0.8, 0.7],
+                    },
+                    flexDirection: "row",
+                  }}
+                >
+                  <h5>{quote.name}</h5>
+                  <h6>~{quote.user.email}</h6>
+                </Box>
+              );
+            })}
         </div>
       </Stack>
     </Container>
